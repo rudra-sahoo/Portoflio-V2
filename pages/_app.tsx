13deca8eb72d522bf74b114065b2f0217bc48cfa
Rudra <rudranarayanaknr@gmail.com>
2025-04-15 16:57:07 +0530

    vercel Speed Insights Added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const poppins = Poppins({ 
   weight: ["100", "200", "400", "700", "900"],
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <div id="root" className={poppins.className}>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights/>
     </div>
   );
 }
